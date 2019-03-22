ca6aad109673c96c9ca4db9474abd9f19d6e448a
Ed Ball <ed.ball@faithlife.com>
2019-03-21 21:04:15 -0700

    Use Faithlife.Build.

---
+++ tools/Build/Build.cs
@@ -0,0 +1,19 @@
+using System;
+using Faithlife.Build;
+
+internal static class Build
+{
+	public static int Main(string[] args) => BuildRunner.Execute(args, build =>
+	{
+		build.AddDotNetTargets(
+			new DotNetBuildSettings
+			{
+				DocsSettings = new DotNetDocsSettings
+				{
+					GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
+					GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
+					SourceCodeUrl = "https://github.com/Faithlife/RepoName/tree/master/src",
+				},
+			});
+	});
+}
