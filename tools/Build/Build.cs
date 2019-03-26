23d530ecf01f6f11b16a5ea3ac4fea39277c8a3d
Ed Ball <ed.ball@faithlife.com>
2019-03-26 12:51:31 -0700

    Add generate-example, verify-example, and default build targets.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -1,19 +1,44 @@
 using System;
 using Faithlife.Build;
+using static Faithlife.Build.AppRunner;
 
 internal static class Build
 {
 	public static int Main(string[] args) => BuildRunner.Execute(args, build =>
 	{
-		build.AddDotNetTargets(
-			new DotNetBuildSettings
+		var dotNetBuildSettings = new DotNetBuildSettings
+		{
+			DocsSettings = new DotNetDocsSettings
 			{
-				DocsSettings = new DotNetDocsSettings
-				{
-					GitLogin = new GitLoginInfo("ejball", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
-					GitAuthor = new GitAuthorInfo("ejball", "ejball@gmail.com"),
-					SourceCodeUrl = "https://github.com/ejball/ArgsReading/tree/master/src",
-				},
-			});
+				GitLogin = new GitLoginInfo("ejball", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
+				GitAuthor = new GitAuthorInfo("ejball", "ejball@gmail.com"),
+				SourceCodeUrl = "https://github.com/ejball/ArgsReading/tree/master/src",
+			},
+		};
+
+		build.AddDotNetTargets(dotNetBuildSettings);
+
+		build.Target("generate-example")
+			.Describe("Generates documentation for ExampleAssembly")
+			.DependsOn("build")
+			.Does(() => generateDocs(verify: false));
+
+		build.Target("verify-example")
+			.Describe("Generates documentation for ExampleAssembly")
+			.DependsOn("build")
+			.Does(() => generateDocs(verify: false));
+
+		build.Target("test")
+			.DependsOn("verify-example");
+
+		build.Target("default").DependsOn("build");
+
+		void generateDocs(bool verify)
+		{
+			var configuration = dotNetBuildSettings.BuildOptions.ConfigurationOption.Value;
+			RunApp($"src/XmlDocMarkdown/bin/{configuration}/XmlDocMarkdown.exe",
+				$"tools/XmlDocTarget/bin/{configuration}/net47/ExampleAssembly.dll", "docs",
+				"--settings", "tools/ExampleAssembly/XmlDocSettings.json", verify ? "--verify" : null);
+		}
 	});
 }
