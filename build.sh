e10839a81b148b5bccebbbfc2c453d51384181a7
Ed Ball <ed.ball@faithlife.com>
2019-03-25 20:49:44 -0700

    Set MONO_ROOT in build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export MONO_ROOT=$(dirname $(which mono))/../
+echo $MONO_ROOT
 dotnet run --project "$SCRIPT_DIR/tools/Build/Build.csproj" -- "$@"
