297edebafe1e00674311bc6089eaea9d27515f89
Ed Ball <ed.ball@faithlife.com>
2019-03-26 20:59:04 -0700

    Use RunDotNetFrameworkApp from Faithlife.Build.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Linq;
-using System.Runtime.InteropServices;
 using Faithlife.Build;
 using static Faithlife.Build.AppRunner;
 
@@ -14,54 +11,29 @@ internal static class Build
 		build.Target("generate-docs")
 			.Describe("Generates documentation")
 			.DependsOn("build")
-			.Does(() => generateDocs(verify: false));
+			.Does(() => GenerateDocs(dotNetBuildSettings, verify: false));
 
 		build.Target("verify-docs")
 			.Describe("Verifies generated documentation")
 			.DependsOn("build")
-			.Does(() => generateDocs(verify: false));
+			.Does(() => GenerateDocs(dotNetBuildSettings, verify: true));
 
 		build.Target("test")
 			.DependsOn("verify-docs");
 
 		build.Target("default").DependsOn("build");
-
-		void generateDocs(bool verify)
-		{
-			var configuration = dotNetBuildSettings.BuildOptions.ConfigurationOption.Value;
-			foreach ((string assemblyPath, string settingsPath) in new[]
-			{
-				($"tools/XmlDocTarget/bin/{configuration}/net47/XmlDocMarkdown.Core.dll", "src/XmlDocMarkdown.Core/XmlDocSettings.json"),
-				($"tools/XmlDocTarget/bin/{configuration}/net47/Cake.XmlDocMarkdown.dll", "src/Cake.XmlDocMarkdown/XmlDocSettings.json"),
-				($"tools/XmlDocTarget/bin/{configuration}/net47/ExampleAssembly.dll", "tools/ExampleAssembly/XmlDocSettings.json"),
-			})
-			{
-				RunDotNetFrameworkApp($"src/XmlDocMarkdown/bin/{configuration}/XmlDocMarkdown.exe",
-					assemblyPath, "docs", "--settings", settingsPath, verify ? "--verify" : null);
-			}
-		}
 	});
 
-	private static void RunDotNetFrameworkApp(string path, params string[] args)
+	private static void GenerateDocs(DotNetBuildSettings dotNetBuildSettings, bool verify)
 	{
-		if (IsPlatform(OSPlatform.OSX) || IsPlatform(OSPlatform.Linux))
+		var configuration = dotNetBuildSettings.BuildOptions.ConfigurationOption.Value;
+		var projects = new[]
 		{
-			args = new[] { path }.Concat(args).ToArray();
-			path = "mono";
-		}
-
-		RunApp(path, args);
-	}
-
-	private static bool IsPlatform(OSPlatform platform)
-	{
-		try
-		{
-			return RuntimeInformation.IsOSPlatform(platform);
-		}
-		catch (PlatformNotSupportedException)
-		{
-			return false;
-		}
+			($"tools/XmlDocTarget/bin/{configuration}/net47/XmlDocMarkdown.Core.dll", "src/XmlDocMarkdown.Core/XmlDocSettings.json"),
+			($"tools/XmlDocTarget/bin/{configuration}/net47/Cake.XmlDocMarkdown.dll", "src/Cake.XmlDocMarkdown/XmlDocSettings.json"),
+			($"tools/XmlDocTarget/bin/{configuration}/net47/ExampleAssembly.dll", "tools/ExampleAssembly/XmlDocSettings.json"),
+		};
+		foreach ((string assemblyPath, string settingsPath) in projects)
+			RunDotNetFrameworkApp($"src/XmlDocMarkdown/bin/{configuration}/XmlDocMarkdown.exe", assemblyPath, "docs", "--settings", settingsPath, verify ? "--verify" : null);
 	}
 }
