245cdee2de276aa6eaceffce1753e4284cb82659
Ed Ball <ed.ball@faithlife.com>
2019-10-30 11:11:07 -0700

    Revert breaking change to build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
+export MONO_ROOT=$(dirname $(which mono))/../
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 dotnet run --project "$SCRIPT_DIR/tools/Build/Build.csproj" -- "$@"
