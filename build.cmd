e3987fd77d11a0dd839ae5d4187fa30c2e52c985
Ed Ball <ed.ball@faithlife.com>
2020-03-03 10:23:36 -0800

    Don't run build script if it failed to publish.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0
 dotnet publish tools\Build\Build.csproj --output tools\bin\Build --nologo --verbosity quiet
-dotnet tools\bin\Build\Build.dll %*
+if %errorlevel% equ 0 dotnet tools\bin\Build\Build.dll %*
 popd
