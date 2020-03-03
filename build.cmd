6af88b4948b36dcb4448c1be5f3e274ee5938c6c
Ed Ball <ed.ball@faithlife.com>
2020-03-03 10:14:09 -0800

    Publish build script quietly.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0
-dotnet publish tools\Build\Build.csproj --output tools\bin\Build
+dotnet publish tools\Build\Build.csproj --output tools\bin\Build --nologo --verbosity quiet
 dotnet tools\bin\Build\Build.dll %*
 popd
