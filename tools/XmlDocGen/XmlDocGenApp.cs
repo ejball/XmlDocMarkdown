c7844704d8250af37037575639c4303a5fffe329
Ed Ball <ed.ball@faithlife.com>
2020-09-03 13:15:35 -0700

    Switch to XmlDocGen model.

---
+++ tools/XmlDocGen/XmlDocGenApp.cs
@@ -0,0 +1,9 @@
+using XmlDocMarkdown.Core;
+
+namespace XmlDocGen
+{
+	internal sealed class XmlDocGenApp
+	{
+		public static int Main(string[] args) => XmlDocMarkdownApp.Run(args);
+	}
+}
--- tools/XmlDocTarget/Program.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace XmlDocTarget
-{
-	internal sealed class Program
-	{
-		private static void Main() => Console.WriteLine("Used for building documentation.");
-	}
-}
