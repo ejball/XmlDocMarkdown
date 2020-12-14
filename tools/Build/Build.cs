588ed52b067783743feafca7c9f839ca53523870
Ed Ball <ed.ball@faithlife.com>
2020-12-14 13:16:22 -0800

    Use C# 9, .NET 5, latest analyzers. Build macOS.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -1,20 +1,14 @@
 using System;
 using Faithlife.Build;
 
-internal static class Build
-{
-	public static int Main(string[] args) => BuildRunner.Execute(args, build =>
+BuildRunner.Execute(args, build => build.AddDotNetTargets(
+	new DotNetBuildSettings
 	{
-		build.AddDotNetTargets(
-			new DotNetBuildSettings
-			{
-				NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
-				DocsSettings = new DotNetDocsSettings
-				{
-					GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
-					GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
-					SourceCodeUrl = "https://github.com/Faithlife/RepoName/tree/master/src",
-				},
-			});
-	});
-}
+		NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
+		DocsSettings = new DotNetDocsSettings
+		{
+			GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
+			GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
+			SourceCodeUrl = "https://github.com/Faithlife/RepoName/tree/master/src",
+		},
+	}));
