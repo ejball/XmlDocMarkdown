304e251f1969ff1e4daf2f4d2e7a560aa73e7dd8
Ed Ball <ed.ball@faithlife.com>
2021-01-12 09:23:54 -0800

    Standardize on build.ps1, since all platforms can run Powershell.

--- build.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-pushd %~dp0
-dotnet publish tools\Build\Build.csproj --output tools\bin\Build --nologo --verbosity quiet
-if %errorlevel% equ 0 dotnet tools\bin\Build\Build.dll %*
-popd
-exit /b %errorlevel%
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-dotnet publish ./tools/Build/Build.csproj --output ./tools/bin/Build --nologo --verbosity quiet
-dotnet exec ./tools/bin/Build/Build.dll "$@"
