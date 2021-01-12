b2378d725079db7705bc89661abe94dbdc4d1a03
Ed Ball <ed.ball@faithlife.com>
2021-01-12 13:27:33 -0800

    Optimize repeated build script invocations.

--- build.ps1
+++ build.ps1
@@ -1,5 +1,14 @@
 Push-Location $PSScriptRoot
-dotnet publish ./tools/Build/Build.csproj --output ./tools/bin/Build --nologo --verbosity quiet
-if ($LASTEXITCODE -eq 0) { dotnet ./tools/bin/Build/Build.dll $args }
-Pop-Location
-exit $LASTEXITCODE
+try {
+  if (-not (Test-Path ./tools/bin/Build) -or
+      (Get-ChildItem ./tools/Build/* | Measure-Object LastWriteTime -Maximum).Maximum -gt
+      (Get-ChildItem ./tools/bin/Build/* | Measure-Object LastWriteTime -Maximum).Maximum) {
+    dotnet publish ./tools/Build/Build.csproj --output ./tools/bin/Build --nologo --verbosity quiet
+    if ($LASTEXITCODE -ne 0) { throw }
+  }
+  dotnet ./tools/bin/Build/Build.dll $args
+}
+finally {
+  Pop-Location
+  exit $LASTEXITCODE
+}
