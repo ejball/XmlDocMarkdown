04554acd2895e12030e8b6bfe446fb745c88eaab
Ed Ball <ed.ball@faithlife.com>
2021-03-15 19:57:41 -0700

    Push git tag when package is published.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -1,14 +1,24 @@
 using System;
 using Faithlife.Build;
 
-return BuildRunner.Execute(args, build => build.AddDotNetTargets(
-	new DotNetBuildSettings
-	{
-		NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
-		DocsSettings = new DotNetDocsSettings
+return BuildRunner.Execute(args, build =>
+{
+	var gitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? "");
+
+	build.AddDotNetTargets(
+		new DotNetBuildSettings
 		{
-			GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
-			GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
-			SourceCodeUrl = "https://github.com/Faithlife/RepoName/tree/master/src",
-		},
-	}));
+			NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
+			DocsSettings = new DotNetDocsSettings
+			{
+				GitLogin = gitLogin,
+				GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
+				SourceCodeUrl = "https://github.com/Faithlife/RepoName/tree/master/src",
+			},
+			PackageSettings = new DotNetPackageSettings
+			{
+				GitLogin = gitLogin,
+				PushTagOnPublish = x => $"v{x.Version}",
+			},
+		});
+});
