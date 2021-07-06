366f8167b6fdd40a1ec315045826fe18f45fdc83
Thomas Baker <thomaslbaker@outlook.com>
2021-07-05 21:42:11 -0400

    Added ExampleRecord C# record to ExampleAssembly

---
+++ tools/ExampleAssembly/ExampleRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExampleAssembly
+{
+	/// <summary>
+	/// A C# 9 generic record&lt;T&gt;.
+	/// </summary>
+	/// <param name="Name">A readonly string</param>
+	/// <param name="Age">A readonly int</param>
+	/// <param name="Days">A Hashset&lt;DayOfWeek&gt;</param>
+	/// <param name="GenericType">A generic type parameter.</param>
+	/// <param name="GenericLambda">An Action&lt;T&gt; lambda parameter.</param>
+	/// <typeparam name="T">Some generic type</typeparam>
+	public record ExampleRecord<T>(string Name, int Age, HashSet<DayOfWeek> Days, T GenericType, Action<T> GenericLambda);
+}
