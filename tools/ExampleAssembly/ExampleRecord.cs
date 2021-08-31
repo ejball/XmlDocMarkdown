a1f0baed4eb062ee9f831fc9e66372a476a44859
Ed Ball <ed.ball@faithlife.com>
2021-08-31 15:26:46 -0700

    Tweak comments.

--- tools/ExampleAssembly/ExampleRecord.cs
+++ tools/ExampleAssembly/ExampleRecord.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 namespace ExampleAssembly
 {
 	/// <summary>
-	/// A C# 9 generic record&lt;T&gt;.
+	/// A C# 9 generic record.
 	/// </summary>
-	/// <param name="Name">A readonly string</param>
-	/// <param name="Age">A readonly int</param>
-	/// <param name="Days">A Hashset&lt;DayOfWeek&gt;</param>
+	/// <param name="Name">A string.</param>
+	/// <param name="Age">An integer.</param>
+	/// <param name="Days">A hash set.</param>
 	/// <param name="GenericType">A generic type parameter.</param>
-	/// <param name="GenericLambda">An Action&lt;T&gt; lambda parameter.</param>
-	/// <typeparam name="T">Some generic type</typeparam>
+	/// <param name="GenericLambda">An lambda parameter.</param>
+	/// <typeparam name="T">Some generic type.</typeparam>
 	public record ExampleRecord<T>(string Name, int Age, HashSet<DayOfWeek> Days, T GenericType, Action<T> GenericLambda);
 }
--- tools/ExampleAssembly/IsExternalInit.cs
+++ tools/ExampleAssembly/IsExternalInit.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 namespace System.Runtime.CompilerServices
 {
 	/// <summary>
-	/// Bug fix for C# 9 record when not using .Net 5
+	/// Bug fix for C# 9 record when not using .NET 5.
 	/// </summary>
 	[EditorBrowsable(EditorBrowsableState.Never)]
 	internal class IsExternalInit
